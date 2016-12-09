d18dec23853db76b3db5c69be610175398bae924
fabio fenoglio <fabio.fenoglio@aizoon.it>
2016-12-09 15:23:21 +0100

    first commit

---
+++ src/com/ff/utils/data/mybatis/plugin/ForceDecimalToDoublePlugin.java
@@ -0,0 +1,36 @@
+package com.ff.utils.data.mybatis.plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+
+public class ForceDecimalToDoublePlugin extends PluginAdapter {
+
+	public ForceDecimalToDoublePlugin() {
+
+	}
+
+	public boolean validate(List<String> warnings) {
+		return true;
+	}
+
+	@Override
+	public void initialized(IntrospectedTable introspectedTable) {
+
+		List<String> toReplace = new ArrayList<String>();
+		toReplace.add("java.math.BigDecimal");
+		String forceTo = "java.lang.Double";
+		
+		for (IntrospectedColumn o : introspectedTable.getAllColumns()) {
+			if (toReplace.contains(o.getFullyQualifiedJavaType().getFullyQualifiedName())) {
+				FullyQualifiedJavaType fullyQualifiedJavaType = new FullyQualifiedJavaType(forceTo);
+				o.setFullyQualifiedJavaType(fullyQualifiedJavaType);
+			}
+		}
+
+	}
+}
---
+++ src/com/ff/utils/data/mybatis/plugin/ForceTypeSubstitutionPlugin.java
@@ -0,0 +1,52 @@
+package com.ff.utils.data.mybatis.plugin;
+
+import java.util.List;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+import static org.mybatis.generator.internal.util.messages.Messages.getString;
+
+public class ForceTypeSubstitutionPlugin extends PluginAdapter {
+
+	private String from;
+	private String to;
+
+	public ForceTypeSubstitutionPlugin() {
+
+	}
+
+	public boolean validate(List<String> warnings) {
+
+		from = properties.getProperty("from");
+		to = properties.getProperty("to");
+
+		boolean valid = stringHasValue(from) && stringHasValue(to);
+
+		if (valid) {
+			// NOP
+		} else {
+			if (!stringHasValue(from)) {
+				warnings.add(getString("ValidationError.18", this.getClass().getSimpleName(), "from"));
+			}
+			if (!stringHasValue(to)) {
+				warnings.add(getString("ValidationError.18", this.getClass().getSimpleName(), "to"));
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void initialized(IntrospectedTable introspectedTable) {
+
+		for (IntrospectedColumn o : introspectedTable.getAllColumns()) {
+			if (o.getFullyQualifiedJavaType().getFullyQualifiedName().equals(from)) {
+				FullyQualifiedJavaType fullyQualifiedJavaType = new FullyQualifiedJavaType(to);
+				o.setFullyQualifiedJavaType(fullyQualifiedJavaType);
+			}
+		}
+
+	}
+}
